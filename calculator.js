9ca502ea66ca245ee224919383e11ae3f75cfebd
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-05 10:37:33 -0500

    Upload project as of 10-5 with simple calculator using NodeJS Express BodyParser

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+const port = 3000;
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", function(req, res){
+  console.log(req.body);
+  var sum = Number(req.body.num1) + Number(req.body.num2);
+  res.send("The sum is " + sum + ".");
+});
+
+app.listen(port, function(){
+  console.log("Starting server on port " + port + "....");
+});
